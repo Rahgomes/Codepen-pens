7b5daa518667f08c21fd9e2aa768daa28a6c756d
Ramon Gomes <ramon_gds@hotmail.com>
2020-12-28 17:16:51 -0300

    pen Botão load more

---
+++ botao load more/script.js
@@ -0,0 +1,18 @@
+$(function (){
+
+    $(".box-hidden").slice(0, 2).show();
+
+    $("#loadMore").on('click', function(e){
+        
+        e.preventDefault();
+
+        $(".box-hidden:hidden").slice(0, 2).slideDown();
+        if ($(".box-hidden:hidden").length == 0) {
+            $("#load").fadeOut('slow');
+        }
+
+        $('html,body').animate({
+            scrollTop: $(this).offset().top
+        }, 1500);
+    });
+});
